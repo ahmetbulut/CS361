2c1372fc7a1ebc3b9a7a2f0912a8f545310d9488
ahmetbulut <ahmetbulut@sehir.edu.tr>
2015-10-22 07:04:58 +0300

    Modified the test logic slightly.

--- test/TestDataProcess.py
+++ test/TestDataProcess.py
@@ -6,7 +6,7 @@ import source.dataprocess as DP
 class MyTestCase(unittest.TestCase):
 
     def testDataProcess(self):
-        self.assertEqual(DP.getwords("<body></body>"),[])
+        self.assertEqual(DP.getwords("<head></head>"),[])
 
 if __name__ == '__main__':
     unittest.main()
